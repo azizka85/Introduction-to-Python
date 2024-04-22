e8bd7ec789f72f7fe1c23cfb6f9e2f6bfb8a5bb7
azizka85 <aziz.kudaikulov@gmail.com>
2024-04-22 12:29:33 +0500

    make static field __country public in CarFabric class

--- modules/car_fabric.py
+++ modules/car_fabric.py
@@ -1,12 +1,12 @@
 from modules.car import Car, CountryCar
 
 class CarFabric:
-    __country = 'Kazakhstan'
+    country: str = 'Kazakhstan'
 
     @classmethod
     def create_car(cls, type: int, name: str, max_speed: int) -> Car:
         match type:
-            case 1: 
-                return CountryCar(name, max_speed, cls.__country)
+            case 1:                 
+                return CountryCar(name, max_speed, cls.country)
             case _:
                 return Car(name, max_speed)
--- modules/person.py
+++ modules/person.py
@@ -1,4 +1,4 @@
-class Person():
+class Person:
     name: str
     email: str
 
